288257e856ee0749ca494529a8c79ff11e65453e
sleduardo20 <sl.eduardo20@gmail.com>
2020-10-20 22:10:05 -0300

    chore: add definitions theme

---
+++ src/styles/theme.ts
@@ -0,0 +1,51 @@
+export default {
+  grid: {
+    container: '130rem',
+    gutter: '3.2rem',
+  },
+  border: {
+    radius: '0.4rem',
+  },
+  font: {
+    family:
+      "Poppins, --apple-system, BlinkMacSystemFont, 'Segoe UI',Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
+    ligth: 300,
+    normal: 400,
+    bold: 600,
+    sizes: {
+      xsmall: '1.2rem',
+      small: '1.4rem',
+      medium: '1.6rem',
+      large: '1.8rem',
+      xlarge: '2.0rem',
+      xxlarge: '2.8rem',
+    },
+  },
+  colors: {
+    primary: '#f231a5',
+    secondary: '#3cd3c1',
+    mainBg: '#06082b',
+    white: '#fafafa',
+    black: '#030517',
+    ligthGray: '#fafafa',
+    gray: '#8f8f8f',
+    darkGray: '#2e2f42',
+  },
+  spacings: {
+    xxsmall: '0.8rem',
+    xsmall: '1.6rem',
+    small: '2.4rem',
+    medium: '3.2rem',
+    large: '4.0rem',
+    xlarge: '4.8rem',
+    xxlarge: '5.6rem',
+    xxxlarge: '6.4rem',
+  },
+  layers: {
+    base: 10,
+    menu: 20,
+    overlay: 30,
+    modal: 40,
+    alwaysOnTop: 50,
+  },
+} as const;
