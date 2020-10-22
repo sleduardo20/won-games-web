abfd5a3f9976b647bf74b48f084d892780d83af0
sleduardo20 <sl.eduardo20@gmail.com>
2020-10-22 20:09:49 -0300

    chore: create tests at styles component Button

--- src/__tests__/components/button.spec.tsx
+++ src/__tests__/components/button.spec.tsx
@@ -1,7 +1,35 @@
-import { render, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
+import { renderWithTheme } from 'utils/tests/helpers';
 
 import Button from '../../components/Button';
 
 describe('<Button />', () => {
-  it('should be open button', () => {});
+  it('should render the medium size by default ', () => {
+    renderWithTheme(<Button>Buy Now</Button>);
+
+    expect(screen.getByRole('button', { name: /Buy Now/i })).toHaveStyle({
+      height: '4rem',
+      padding: '0.8rem 3.2rem',
+      'font-size': '1.4rem',
+    });
+  });
+
+  it('should render the small size by default ', () => {
+    renderWithTheme(<Button size="small">Buy Now</Button>);
+
+    expect(screen.getByRole('button', { name: /Buy Now/i })).toHaveStyle({
+      height: '3rem',
+      'font-size': '1.2rem',
+    });
+  });
+
+  it('should render the large size by default ', () => {
+    renderWithTheme(<Button size="large">Buy Now</Button>);
+
+    expect(screen.getByRole('button', { name: /Buy Now/i })).toHaveStyle({
+      height: '5rem',
+      padding: '0.8rem 4.8rem',
+      'font-size': '1.6rem',
+    });
+  });
 });
