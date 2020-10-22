0572cb6c36048d1cc31a7576de9bf1d3a800ebea
sleduardo20 <sl.eduardo20@gmail.com>
2020-10-21 22:22:23 -0300

    chore: test to mobile component Logo

--- src/components/Logo/styles.ts
+++ src/components/Logo/styles.ts
@@ -1,4 +1,5 @@
 import styled, { css } from 'styled-components';
+import media from 'styled-media-query';
 
 import { LogoProps } from '.';
 
@@ -11,12 +12,28 @@ const containerModifiers = {
     width: 20rem;
     height: 5.9rem;
   `,
+  hideOnMobile: () => css`
+    ${media.lessThan('medium')`
+      width: 5.8rem;
+      height: 4.5rem;
+
+      svg {
+        height: 4.5rem;
+        pointer-events: none;
+      }
+
+      .text-mobile{
+        display: none;
+      }
+    `}
+  `,
 };
 
 export const Container = styled.div<LogoProps>`
-  ${({ theme, color, size }) => css`
+  ${({ theme, color, size, hideOnMobile }) => css`
     color: ${theme.colors[color!]};
 
     ${!!size && containerModifiers[size]}
+    ${hideOnMobile && containerModifiers.hideOnMobile}
   `}
 `;
