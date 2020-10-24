20af324364f7ac967777d72990b9abbd92363e56
sleduardo20 <sl.eduardo20@gmail.com>
2020-10-24 11:11:36 -0300

    chore: finish styles component Ribbon ajusts

--- src/components/HighLight/styles.ts
+++ src/components/HighLight/styles.ts
@@ -2,54 +2,6 @@ import styled, { css } from 'styled-components';
 import media from 'styled-media-query';
 import { HighLightProps } from '.';
 
-type ContainerProps = Pick<HighLightProps, 'backgroundImage' | 'alignment'>;
-
-const containerModfiers = {
-  right: () => css`
-    grid-template-areas: 'floatimage content';
-    grid-template-columns: 1.3fr 2fr;
-    ${Content} {
-      text-align: right;
-    }
-  `,
-  left: () => css`
-    grid-template-areas: 'content floatimage';
-    grid-template-columns: 2fr 1.3fr;
-    ${Content} {
-      text-align: left;
-    }
-
-    ${FloatImage} {
-      justify-self: end;
-    }
-  `,
-};
-
-export const Container = styled.section<ContainerProps>`
-  ${({ backgroundImage, alignment }) => css`
-    position: relative;
-    background-image: url(${backgroundImage});
-    background-position: center center;
-    background-size: cover;
-    height: 23rem;
-    display: grid;
-
-    ${media.greaterThan('medium')`
-      height: 32rem;
-    `}
-
-    ${containerModfiers[alignment!]}
-
-    &::after {
-      content: '';
-      position: absolute;
-      width: 100%;
-      height: 100%;
-      background-color: rgba(0, 0, 0, 0.6);
-    }
-  `}
-`;
-
 export const Content = styled.div`
   ${({ theme }) => css`
     grid-area: content;
@@ -99,5 +51,53 @@ export const SubTitle = styled.h3`
     ${media.greaterThan('medium')`
       font-size: ${theme.font.sizes.large};
     `}
+  `}
+`;
+
+type ContainerProps = Pick<HighLightProps, 'backgroundImage' | 'alignment'>;
+
+const containerModfiers = {
+  right: () => css`
+    grid-template-areas: 'floatimage content';
+    grid-template-columns: 1.3fr 2fr;
+    ${Content} {
+      text-align: right;
+    }
+  `,
+  left: () => css`
+    grid-template-areas: 'content floatimage';
+    grid-template-columns: 2fr 1.3fr;
+    ${Content} {
+      text-align: left;
+    }
+
+    ${FloatImage} {
+      justify-self: end;
+    }
+  `,
+};
+
+export const Container = styled.section<ContainerProps>`
+  ${({ backgroundImage, alignment }) => css`
+    position: relative;
+    background-image: url(${backgroundImage});
+    background-position: center center;
+    background-size: cover;
+    height: 23rem;
+    display: grid;
+
+    ${media.greaterThan('medium')`
+      height: 32rem;
+    `}
+
+    ${containerModfiers[alignment!]}
+
+    &::after {
+      content: '';
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      background-color: rgba(0, 0, 0, 0.6);
+    }
   `}
 `;
