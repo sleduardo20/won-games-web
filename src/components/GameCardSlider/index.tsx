2136f0745a6f4a51baa5992cb62b799901056b2b
sleduardo20 <sl.eduardo20@gmail.com>
2020-10-26 12:55:37 -0300

    chore: create component GameCardSlider

---
+++ src/components/GameCardSlider/index.tsx
@@ -0,0 +1,59 @@
+import {
+  ArrowBackIos as ArrowLeft,
+  ArrowForwardIos as ArrowRight,
+} from 'styled-icons/material-outlined';
+
+import Slider, { SliderSettings } from '../Slider';
+import GameCard, { GameCardProps } from '../GameCard';
+
+import { Container } from './styles';
+
+export interface GameCardSliderProps {
+  items: GameCardProps[];
+  color?: 'white' | 'black';
+}
+
+const settings: SliderSettings = {
+  infinite: false,
+  lazyLoad: 'ondemand',
+  nextArrow: <ArrowRight aria-label="next games" />,
+  prevArrow: <ArrowLeft aria-label="previews games" />,
+  slidesToShow: 4,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        arrows: false,
+        slidesToShow: 2.2,
+      },
+    },
+    {
+      breakpoint: 570,
+      settings: {
+        arrows: false,
+        slidesToShow: 1.2,
+      },
+    },
+    {
+      breakpoint: 375,
+      settings: {
+        arrows: false,
+        slidesToShow: 1.1,
+      },
+    },
+  ],
+};
+
+const GameCardSlider = ({ items, color = 'black' }: GameCardSliderProps) => {
+  return (
+    <Container color={color}>
+      <Slider settings={settings}>
+        {items.map(item => (
+          <GameCard key={item.title} {...item} />
+        ))}
+      </Slider>
+    </Container>
+  );
+};
+
+export default GameCardSlider;
