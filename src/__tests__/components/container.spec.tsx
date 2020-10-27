05698aa2d0d494e24978499cdbb7e50813aff192
sleduardo20 <sl.eduardo20@gmail.com>
2020-10-27 11:55:11 -0300

    chore: create  Container component with TDD

---
+++ src/__tests__/components/container.spec.tsx
@@ -0,0 +1,28 @@
+import { Container } from 'components/Container';
+import theme from 'styles/theme';
+import { renderWithTheme } from 'utils/tests/helpers';
+
+describe('<Container />', () => {
+  it('should be able the render Container component correctly', () => {
+    const { container } = renderWithTheme(<Container />);
+
+    expect(container.firstChild).toHaveStyleRule(
+      'max-width',
+      theme.grid.container,
+    );
+
+    expect(container.firstChild).toMatchInlineSnapshot(`
+      .c0 {
+        max-width: 130rem;
+        margin-left: auto;
+        margin-right: auto;
+        padding-left: calc(3.2rem / 2);
+        padding-right: calc(3.2rem / 2);
+      }
+
+      <div
+        class="c0"
+      />
+    `);
+  });
+});
