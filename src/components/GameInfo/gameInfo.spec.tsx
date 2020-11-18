a429e8235a0348e7772e6f95446ae20f615729ee
sleduardo20 <sl.eduardo20@gmail.com>
2020-11-18 12:19:53 -0300

    styleding the GameInfo component

--- src/components/GameInfo/gameInfo.spec.tsx
+++ src/components/GameInfo/gameInfo.spec.tsx
@@ -11,13 +11,17 @@ const props = {
 
 describe('<GameInfo/>', () => {
   it('should be able render GameInfo component correctly', () => {
-    renderWithTheme(<GameInfo {...props} />);
+    const { container } = renderWithTheme(<GameInfo {...props} />);
 
     expect(
       screen.getByRole('heading', { name: /my game title/i }),
     ).toBeInTheDocument();
+
     expect(screen.getByText(/my description/i)).toBeInTheDocument();
+
     expect(screen.getByText(/\$210.00/i)).toBeInTheDocument();
+
+    expect(container.firstChild).toMatchSnapshot();
   });
 
   it('should be able render buttons GameInfo', () => {
--- src/stories/20-GameInfo.stories.tsx
+++ src/stories/20-GameInfo.stories.tsx
@@ -2,9 +2,21 @@ import { Meta, Story } from '@storybook/react/types-6-0';
 
 import GameInfo, { GameInfoProps } from '../components/GameInfo';
 
+import mockGameInfo from '../components/GameInfo/mock';
+
 export default {
   title: 'GameInfo',
   component: GameInfo,
+  args: mockGameInfo,
+  parameters: {
+    backgrounds: {
+      default: 'won-dark',
+    },
+  },
 } as Meta;
 
-export const Basic: Story<GameInfoProps> = args => <GameInfo {...args} />;
+export const Basic: Story<GameInfoProps> = args => (
+  <div style={{ maxWidth: '144rem', margin: '0 auto' }}>
+    <GameInfo {...args} />
+  </div>
+);
