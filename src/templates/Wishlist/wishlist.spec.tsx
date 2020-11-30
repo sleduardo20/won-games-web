f753897812f92a4cbd46bab0637fe1e589095720
sleduardo20 <sl.eduardo20@gmail.com>
2020-11-30 12:29:41 -0300

    create first tests template Wishlist

--- src/templates/Wishlist/wishlist.spec.tsx
+++ src/templates/Wishlist/wishlist.spec.tsx
@@ -1,10 +1,29 @@
 import { screen } from '@testing-library/react';
 import { renderWithTheme } from 'utils/tests/helpers';
 
-import Wishtlist from '.';
+import mockGames from 'components/GameCardSlider/mock';
+import mockHightLight from 'components/HighLight/mock';
+import Wishtlist, { WishListTemplateProps } from '.';
+
+const props: WishListTemplateProps = {
+  recommendedGames: mockGames,
+  recommendedHighLight: mockHightLight,
+};
+
+jest.mock('components/ShowCase', () => ({
+  __esModule: true,
+  default: function Mock() {
+    return <div data-testid="Mock ShowCase" />;
+  },
+}));
 
 describe('<Wishtlist/>', () => {
   it('should be able render Wishlist', () => {
-    // renderWithTheme(<Wishtlist />);
+    renderWithTheme(<Wishtlist {...props} />);
+
+    expect(screen.getByTestId('Mock ShowCase')).toBeInTheDocument();
+    expect(
+      screen.getByRole('heading', { name: /wishlist/i }),
+    ).toBeInTheDocument();
   });
 });
