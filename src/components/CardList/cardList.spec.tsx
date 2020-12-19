2823f99ff742848d59e0358651c79cacf18a395f
sleduardo20 <sl.eduardo20@gmail.com>
2020-12-19 10:50:42 -0300

    (test) ✔ : create tests CardList component

--- src/components/CardList/cardList.spec.tsx
+++ src/components/CardList/cardList.spec.tsx
@@ -2,9 +2,16 @@ import { screen } from '@testing-library/react';
 import { renderWithTheme } from 'utils/tests/helpers';
 
 import CardList from '.';
+import mockCardList from './mock';
 
 describe('<CardList />', () => {
   it('should be able render correctly', () => {
-    // renderWithTheme(<CardList />);
+    const { container } = renderWithTheme(
+      <CardList items={mockCardList} total="R$ 330,00" />,
+    );
+
+    expect(screen.getAllByRole('heading', { level: 3 })).toHaveLength(2);
+    expect(screen.getByText('R$ 330,00')).toHaveStyle({ color: '#f231a5' });
+    expect(container.firstChild).toMatchSnapshot();
   });
 });
