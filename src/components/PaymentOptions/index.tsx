3dd68a7f7475af9e2da1ffa62612051d25edc502
sleduardo20 <sl.eduardo20@gmail.com>
2020-12-19 11:20:52 -0300

    (feat)🧠: structure component PaymentOption

---
+++ src/components/PaymentOptions/index.tsx
@@ -0,0 +1,47 @@
+import Heading from 'components/Heading';
+import Radio from 'components/Radio';
+import {
+  Container,
+  Body,
+  Footer,
+  CardsList,
+  CardItem,
+  CardInfo,
+} from './styles';
+
+export type PaymentCard = {
+  number: string;
+  flag: string;
+  img: string;
+};
+
+export interface PaymentOptionsProps {
+  cards?: PaymentCard[];
+  handlePayment: () => void;
+}
+
+const PaymentOptions = ({ cards, handlePayment }: PaymentOptionsProps) => {
+  return (
+    <Container>
+      <Body>
+        <Heading color="black" size="small" lineBottom>
+          Payment
+        </Heading>
+        <CardsList>
+          {cards?.map(card => (
+            <CardItem key={card.number}>
+              <CardInfo>
+                <img src={card.img} alt={card.flag} />
+                {card.number}
+              </CardInfo>
+              <Radio id={card.number} name="credit-card" value={card.number} />
+            </CardItem>
+          ))}
+        </CardsList>
+      </Body>
+      <Footer />
+    </Container>
+  );
+};
+
+export default PaymentOptions;
---
+++ src/components/PaymentOptions/styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Container = styled.div``;
+
+export const Body = styled.div``;
+
+export const Footer = styled.div``;
+
+export const CardsList = styled.div``;
+
+export const CardItem = styled.div``;
+
+export const CardInfo = styled.div``;
