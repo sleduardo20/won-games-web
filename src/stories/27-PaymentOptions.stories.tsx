746bbdc0e0b164ac8891187386c9e991e11d68dc
sleduardo20 <sl.eduardo20@gmail.com>
2020-12-30 16:42:59 -0300

    (docs)📑: alter background and definition the width in stories PaymentOption component

--- src/stories/27-PaymentOptions.stories.tsx
+++ src/stories/27-PaymentOptions.stories.tsx
@@ -8,11 +8,18 @@ import mockPaymentOptions from '../components/PaymentOptions/mock';
 export default {
   title: 'components/PaymentOptions',
   component: PaymentOptions,
+  parameters: {
+    backgrounds: {
+      default: 'won-dark',
+    },
+  },
   args: {
     cards: mockPaymentOptions,
   },
 } as Meta;
 
 export const Basic: Story<PaymentOptionsProps> = args => (
-  <PaymentOptions {...args} />
+  <div style={{ maxWidth: '400', padding: 16 }}>
+    <PaymentOptions {...args} />
+  </div>
 );
