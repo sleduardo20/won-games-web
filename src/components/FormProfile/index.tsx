c75aa611ff400130f9e2e315d4bceb5b0db4f6ad
sleduardo20 <sl.eduardo20@gmail.com>
2021-01-01 10:16:20 -0300

    (feat)🧠: developing FormProfile component with TDD, implementarion of test, expect input box

--- src/components/FormProfile/index.tsx
+++ src/components/FormProfile/index.tsx
@@ -1,5 +1,6 @@
+import TextField from '../TextField';
 import Heading from '../Heading';
-import { Container } from './styles';
+import { Container, Form } from './styles';
 
 const FormProfile = () => {
   return (
@@ -7,6 +8,14 @@ const FormProfile = () => {
       <Heading lineBottom color="black" size="small">
         My Profile
       </Heading>
+      <Form>
+        <TextField
+          name="name"
+          placeholder="Name"
+          label="Name"
+          initialValue="John Cage"
+        />
+      </Form>
     </Container>
   );
 };
