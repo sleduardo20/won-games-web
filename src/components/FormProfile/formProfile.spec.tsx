7676c7dcb27465ebddb472e3e7a480dd2a2b9849
sleduardo20 <sl.eduardo20@gmail.com>
2021-01-01 09:56:06 -0300

    (feat)🧠: developing FormProfile component with TDD, create test render profile

---
+++ src/components/FormProfile/formProfile.spec.tsx
@@ -0,0 +1,14 @@
+import { screen } from '@testing-library/react';
+import { renderWithTheme } from 'utils/tests/helpers';
+
+import FormProfile from '.';
+
+describe('<FormProfile/>', () => {
+  it('should be able render the profile form correctly', () => {
+    renderWithTheme(<FormProfile />);
+
+    expect(
+      screen.getByRole('heading', { name: /my profile/i }),
+    ).toBeInTheDocument();
+  });
+});
---
+++ src/components/FormProfile/index.tsx
@@ -0,0 +1,11 @@
+import { Container } from './styles';
+
+const FormProfile = () => {
+  return (
+    <Container>
+      <h1>FormProfile</h1>
+    </Container>
+  );
+};
+
+export default FormProfile;
