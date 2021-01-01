15f78f3cad02d7795fc7974be68d74adc0c981ff
sleduardo20 <sl.eduardo20@gmail.com>
2021-01-01 10:19:07 -0300

    (test)✔: developing FormProfile component with TDD, create a test in that expect a input box of email

--- src/components/FormProfile/formProfile.spec.tsx
+++ src/components/FormProfile/formProfile.spec.tsx
@@ -11,5 +11,8 @@ describe('<FormProfile/>', () => {
       screen.getByRole('heading', { name: /my profile/i }),
     ).toBeInTheDocument();
     expect(screen.getByRole('textbox', { name: /name/i })).toBeInTheDocument();
+    expect(
+      screen.getByRole('textbox', { name: /e-mail/i }),
+    ).toBeInTheDocument();
   });
 });
