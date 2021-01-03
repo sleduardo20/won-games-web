49867522551568c7a06d2ccf86d604292ccbc30c
sleduardo20 <sl.eduardo20@gmail.com>
2021-01-03 11:50:45 -0300

    (tests)✔: create tests at Profile template

--- src/templates/Profile/index.tsx
+++ src/templates/Profile/index.tsx
@@ -1,4 +1,4 @@
-import { useRouter } from 'next/dist/client/router';
+import { useRouter } from 'next/router';
 
 import { Container } from 'components/Container';
 import Heading from 'components/Heading';
