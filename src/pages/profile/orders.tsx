3b2979c14650cb1e3e644d0b961543cf396d845c
sleduardo20 <sl.eduardo20@gmail.com>
2021-01-03 11:13:10 -0300

    (feat)🧠: create page profile/orders

---
+++ src/pages/profile/orders.tsx
@@ -0,0 +1,23 @@
+import { GetServerSideProps } from 'next';
+
+import OrdersList, { OrdersListProps } from 'components/OrdersList';
+
+import Profile from '../../templates/Profile';
+
+import itemsMock from '../../components/OrdersList/mock';
+
+export default function Orders({ items }: OrdersListProps) {
+  return (
+    <Profile>
+      <OrdersList items={items} />
+    </Profile>
+  );
+}
+
+export const getServerSideProps: GetServerSideProps<OrdersListProps> = async () => {
+  return {
+    props: {
+      items: itemsMock,
+    },
+  };
+};
