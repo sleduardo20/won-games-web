cf13b50735987cacc2cabd22a6cfe1b129ee36ef
sleduardo20 <sl.eduardo20@gmail.com>
2021-02-08 20:28:49 -0300

    (feat)🧠: create structure and styles page Games

--- src/pages/games.tsx
+++ src/pages/games.tsx
@@ -1,5 +1,9 @@
 import { GetServerSideProps } from 'next';
+
 import GamesTemplate, { GamesTemplatesProps } from 'templates/Games';
+
+import filterItemsMock from '../components/ExploreSideBar/mock';
+import gamesCardMock from '../components/GameCardSlider/mock';
 
 export default function GamesPage(props: GamesTemplatesProps) {
   return <GamesTemplate {...props} />;
@@ -8,7 +12,8 @@ export default function GamesPage(props: GamesTemplatesProps) {
 export const getServerSideProps: GetServerSideProps<GamesTemplatesProps> = async () => {
   return {
     props: {
-      games: [],
+      games: gamesCardMock,
+      filterItems: filterItemsMock,
     },
   };
 };
--- src/templates/Games/index.tsx
+++ src/templates/Games/index.tsx
@@ -1,16 +1,38 @@
-import { GameCardProps } from 'components/GameCard';
+import { KeyboardArrowDown } from '@styled-icons/material-outlined';
 
-import { Container } from './styles';
+import Base from 'templates/Base';
+
+import ExploreSideBar, { ItemProps } from 'components/ExploreSideBar';
+import GameCard, { GameCardProps } from 'components/GameCard';
+import { Grid } from 'components/Grid';
+
+import { Main, ShowMore } from './styles';
 
 export interface GamesTemplatesProps {
   games?: GameCardProps[];
+  filterItems: ItemProps[];
 }
 
-const GamesTemplate = ({ games }: GamesTemplatesProps) => {
+const GamesTemplate = ({ games, filterItems }: GamesTemplatesProps) => {
   return (
-    <Container>
-      <h1>Games</h1>
-    </Container>
+    <Base>
+      <Main>
+        <ExploreSideBar items={filterItems} onFilter={() => 'FilterItems'} />
+
+        <section>
+          <Grid>
+            {games?.map(item => (
+              <GameCard key={item.title} {...item} />
+            ))}
+          </Grid>
+
+          <ShowMore role="button" onClick={() => 'ShowMore'}>
+            <p>Show More</p>
+            <KeyboardArrowDown size={35} />
+          </ShowMore>
+        </section>
+      </Main>
+    </Base>
   );
 };
 
