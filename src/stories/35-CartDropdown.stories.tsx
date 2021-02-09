272d92028d4608f8bd10d9e7c020386e0454655f
sleduardo20 <sl.eduardo20@gmail.com>
2021-02-09 14:03:48 -0300

    (docs)📑: add storie CartDropdown component

---
+++ src/stories/35-CartDropdown.stories.tsx
@@ -0,0 +1,25 @@
+import { Story, Meta } from '@storybook/react/types-6-0';
+
+import items from '../components/CartList/mock';
+
+import CartDropdown, { CartDropdownProps } from '../components/CartDropdown';
+
+export default {
+  title: 'components/CartDropdown',
+  component: CartDropdown,
+  args: {
+    items,
+    total: 'R$ 330,00',
+  },
+  parameters: {
+    backgrounds: {
+      default: 'won-dark',
+    },
+  },
+} as Meta;
+
+export const Basic: Story<CartDropdownProps> = args => (
+  <div style={{ maxWidth: '98%', display: 'flex', justifyContent: 'flex-end' }}>
+    <CartDropdown {...args} />
+  </div>
+);
