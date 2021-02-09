5af20f31e46958bff5db694b79ee8fe248e1aeae
sleduardo20 <sl.eduardo20@gmail.com>
2021-02-09 10:32:50 -0300

    (styles)🎨: add styles for Dropdown component

--- src/components/Dropdown/styles.ts
+++ src/components/Dropdown/styles.ts
@@ -1,7 +1,38 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  width: max-content;
+  position: relative;
+`;
 
-export const Title = styled.div``;
+export const Title = styled.div`
+  ${({ theme }) => css`
+    cursor: pointer;
+    color: ${theme.colors.white};
+    display: flex;
+    align-items: center;
+    padding-right: ${theme.spacings.small};
+  `}
+`;
 
-export const Content = styled.div``;
+export const Content = styled.div`
+  ${({ theme }) => css`
+    display: flex;
+    flex-direction: column;
+    background: ${theme.colors.white};
+    color: ${theme.colors.black};
+    margin-top: ${theme.spacings.small};
+    position: absolute;
+    right: 0;
+
+    &::before {
+      content: '';
+      position: absolute;
+      top: -1.2rem;
+      right: 2.4rem;
+      border-right: 1.2rem solid transparent;
+      border-left: 1.2rem solid transparent;
+      border-bottom: 1.2rem solid ${theme.colors.white};
+    }
+  `}
+`;
