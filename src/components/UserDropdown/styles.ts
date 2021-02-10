1ff95a94fae427d4c5ecc5ec4e32af79ad35efec
sleduardo20 <sl.eduardo20@gmail.com>
2021-02-10 12:06:22 -0300

    (styles)🎨: add styles in UserDropdown component

--- src/components/UserDropdown/styles.ts
+++ src/components/UserDropdown/styles.ts
@@ -1,5 +1,39 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export const Nav = styled.nav``;
+export const Nav = styled.nav`
+  ${({ theme }) => css`
+    width: 26rem;
+    display: flex;
+    flex-direction: column;
 
-export const Option = styled.a``;
+    a :not(:last-child) {
+      border-bottom: 0.1rem solid ${theme.colors.ligthGray};
+    }
+  `}
+`;
+
+export const Option = styled.a`
+  ${({ theme }) => css`
+    display: flex;
+    align-items: center;
+    text-decoration: none;
+    background: ${theme.colors.white};
+    color: ${theme.colors.black};
+    padding: ${theme.spacings.xsmall} ${theme.spacings.small};
+    transition: background, color, ${theme.transition.default};
+
+    &:hover {
+      background: ${theme.colors.primary};
+      color: ${theme.colors.white};
+    }
+
+    > svg {
+      width: 2.4rem;
+      height: 2.4rem;
+    }
+
+    > span {
+      margin-left: ${theme.spacings.xsmall};
+    }
+  `}
+`;
