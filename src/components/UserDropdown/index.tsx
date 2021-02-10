64037d680fa69f5f49071efedad8be921cb5c362
sleduardo20 <sl.eduardo20@gmail.com>
2021-02-10 09:17:03 -0300

    (docs) 📑: create story UserDropdown component

--- src/components/UserDropdown/index.tsx
+++ src/components/UserDropdown/index.tsx
@@ -1,11 +1,13 @@
-import { Container } from './styles';
+import { AccountCircle } from '@styled-icons/material-outlined';
 
-const UserDropdown = () => {
-  return (
-    <Container>
-      <h1>Ok</h1>
-    </Container>
-  );
+import Dropdown from '../Dropdown';
+
+export interface UserDropdownProps {
+  username: string;
+}
+
+const UserDropdown = ({ username }: UserDropdownProps) => {
+  return <Dropdown title={username}>content</Dropdown>;
 };
 
 export default UserDropdown;
