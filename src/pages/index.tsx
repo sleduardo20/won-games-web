6437a4d8424377d3341bb99e2d4e5b891167e768
sleduardo20 <sl.eduardo20@gmail.com>
2021-02-10 23:15:09 -0300

    (chore)🚚 : add comments on NextJS methods

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -23,3 +23,12 @@ export const getServerSideProps: GetServerSideProps = async () => {
     },
   };
 };
+
+// os metodos getStaticProps/getServerSideProps só funcionam em pages
+
+// getStaticProps     => gera o estático em build time
+// getServerSideProps => gera via ssr a cada request (código gerado é
+//                       gerado no servidor nunca vai para o client)
+// getInitialProps    => gera via ssr a cada request (o código gerado
+//                       vai para o client, faz um hidratação do lado
+//                       do client depois do primeiro request )
