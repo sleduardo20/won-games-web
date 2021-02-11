6f80d4587247ff93ca19fdd872fcd4cd1f6db355
sleduardo20 <sl.eduardo20@gmail.com>
2021-02-10 23:03:13 -0300

    (feat)⚙: config apolloProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,27 +1,36 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import { ThemeProvider } from 'styled-components';
+
 import GlobalStyles from 'styles/GlobalStyles';
 
 import theme from '../styles/theme';
 
 function App({ Component, pageProps }: AppProps) {
-  return (
-    <ThemeProvider theme={theme}>
-      <Head>
-        <title>Won Games</title>
-        <link rel="shortcut icon" href="/img/icon-512.png" />
-        <link rel="apple-touch-icon" href="/img/icon-512.png" />
-        <link rel="manifest" href="/manifest.json" />
-        <meta
-          name="description"
-          content="The best Game Stories in the world!"
-        />
-      </Head>
+  const client = new ApolloClient({
+    uri: 'http://localhost:1337/graphql',
+    cache: new InMemoryCache(),
+  });
 
-      <Component {...pageProps} />
-      <GlobalStyles />
-    </ThemeProvider>
+  return (
+    <ApolloProvider client={client}>
+      <ThemeProvider theme={theme}>
+        <Head>
+          <title>Won Games</title>
+          <link rel="shortcut icon" href="/img/icon-512.png" />
+          <link rel="apple-touch-icon" href="/img/icon-512.png" />
+          <link rel="manifest" href="/manifest.json" />
+          <meta
+            name="description"
+            content="The best Game Stories in the world!"
+          />
+        </Head>
+
+        <Component {...pageProps} />
+        <GlobalStyles />
+      </ThemeProvider>
+    </ApolloProvider>
   );
 }
 
