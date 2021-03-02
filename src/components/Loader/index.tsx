8a2c00d15fbfaa7cce7b3794d0992089faeb1c8b
sleduardo20 <sl.eduardo20@gmail.com>
2021-03-02 13:56:21 -0300

    (feat)🧠: create Loader component

---
+++ src/components/Loader/index.tsx
@@ -0,0 +1,32 @@
+import styled, { css } from 'styled-components';
+
+export const Loader = styled.div`
+  ${({ theme }) => css`
+    border: 16px solid ${theme.colors.white};
+    border-radius: 50%;
+    border-top: 16px solid ${theme.colors.primary};
+    width: 120px;
+    height: 120px;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    -webkit-animation: spin 1.3s linear infinite;
+    animation: spin 1.3s linear infinite;
+    @-webkit-keyframes spin {
+      0% {
+        -webkit-transform: rotate(0deg);
+      }
+      100% {
+        -webkit-transform: rotate(360deg);
+      }
+    }
+    @keyframes spin {
+      0% {
+        transform: rotate(0deg);
+      }
+      100% {
+        transform: rotate(360deg);
+      }
+    }
+  `}
+`;
