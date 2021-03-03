86ce28900f74b6d1fcdfdb2a8cf6887be2428040
sleduardo20 <sl.eduardo20@gmail.com>
2021-03-03 16:47:18 -0300

    (fix)🧯: alter styles Loader component

--- src/components/Loader/index.tsx
+++ src/components/Loader/index.tsx
@@ -12,14 +12,7 @@ export const Loader = styled.div`
     left: 50%;
     -webkit-animation: spin 1.3s linear infinite;
     animation: spin 1.3s linear infinite;
-    @-webkit-keyframes spin {
-      0% {
-        -webkit-transform: rotate(0deg);
-      }
-      100% {
-        -webkit-transform: rotate(360deg);
-      }
-    }
+
     @keyframes spin {
       0% {
         transform: rotate(0deg);
