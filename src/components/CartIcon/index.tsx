92bbeeaaf2c356eeca93082e91aa3cb6fd12fd14
sleduardo20 <sl.eduardo20@gmail.com>
2021-04-28 15:00:19 -0300

    ⚡ feat (CartIcon, component): add hook useCart with property quantity

--- src/components/CartIcon/index.tsx
+++ src/components/CartIcon/index.tsx
@@ -1,12 +1,10 @@
 import { ShoppingCart } from '@styled-icons/material-outlined';
+import { useCart } from 'hooks/useCart';
 
 import { Container, Badge } from './styles';
 
-export interface CartIconProps {
-  quantity?: number;
-}
-
-const CartIcon = ({ quantity = 0 }: CartIconProps) => {
+const CartIcon = () => {
+  const { quantity } = useCart();
   return (
     <Container>
       {quantity > 0 && <Badge aria-label="cart items">{quantity}</Badge>}
