79c1c1d1c3024578193eefe7e696c88fe2cc3185
sleduardo20 <sl.eduardo20@gmail.com>
2021-04-29 00:27:21 -0300

    ♻ refactor (CartDropdown component): remove properties

--- src/components/CartDropdown/index.tsx
+++ src/components/CartDropdown/index.tsx
@@ -1,20 +1,14 @@
 import CartIcon from '../CartIcon';
 import CartList from '../CartList';
 import Dropdown from '../Dropdown';
-import { GameItemProps } from '../GameItem';
 
 import { Container } from './styles';
 
-export interface CartDropdownProps {
-  items?: GameItemProps[];
-  total?: string;
-}
-
-const CartDropdown = ({ items, total }: CartDropdownProps) => {
+const CartDropdown = () => {
   return (
     <Container>
-      <Dropdown title={<CartIcon quantity={items?.length} />}>
-        <CartList items={items} total={total} hasButton />
+      <Dropdown title={<CartIcon />}>
+        <CartList hasButton />
       </Dropdown>
     </Container>
   );
