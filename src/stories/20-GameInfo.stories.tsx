14fff56ba9a4711a7a90c0395fd608dcb83615d8
sleduardo20 <sl.eduardo20@gmail.com>
2021-04-29 10:16:16 -0300

    📄 docs (GameInfo component): add story IsInCart

--- src/stories/20-GameInfo.stories.tsx
+++ src/stories/20-GameInfo.stories.tsx
@@ -1,4 +1,5 @@
 import { Meta, Story } from '@storybook/react/types-6-0';
+import { CartContextData } from '../hooks/useCart';
 
 import GameInfo, { GameInfoProps } from '../components/GameInfo';
 
@@ -20,3 +21,13 @@ export const Basic: Story<GameInfoProps> = args => (
     <GameInfo {...args} />
   </div>
 );
+
+export const IsInCart: Story<GameInfoProps & CartContextData> = args => (
+  <div style={{ maxWidth: '144rem', margin: '0 auto' }}>
+    <GameInfo {...args} />
+  </div>
+);
+
+IsInCart.args = {
+  isInCart: () => true,
+};
