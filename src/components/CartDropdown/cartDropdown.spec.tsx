4331abea2a46efa29eb777e4acbc3c5203bf3907
sleduardo20 <sl.eduardo20@gmail.com>
2021-04-29 00:26:14 -0300

    ✔ test (CartDropdown component): modify tests, add context at hook useCart

--- src/components/CartDropdown/cartDropdown.spec.tsx
+++ src/components/CartDropdown/cartDropdown.spec.tsx
@@ -1,3 +1,4 @@
+import { defaultValuesCartContext } from 'hooks/useCart';
 import { screen, render } from '../../utils/test-utils';
 
 import CartDropdown from '.';
@@ -5,16 +6,23 @@ import CartDropdown from '.';
 import items from '../CartList/mock';
 
 describe('<CartDropdown />', () => {
-  it('should be able render CartIcon and is badge', () => {
-    render(<CartDropdown items={items} total="R$ 300,00" />);
+  beforeEach(() => {
+    const cartProviderProps = {
+      ...defaultValuesCartContext,
+      items,
+      quantity: items.length,
+      total: 'R$ 300,00',
+    };
 
+    render(<CartDropdown />, { cartProviderProps });
+  });
+
+  it('should be able render CartIcon and is badge', () => {
     expect(screen.getByLabelText(/shopping cart/i)).toBeInTheDocument();
     expect(screen.getByText(`${items.length}`)).toBeInTheDocument();
   });
 
   it('should be able render Dropdown content with cart items and total', () => {
-    render(<CartDropdown items={items} total="R$ 300,00" />);
-
     expect(screen.getByText('R$ 300,00')).toBeInTheDocument();
     expect(screen.getByText(`${items[0].title}`)).toBeInTheDocument();
   });
