7802540c97e810afeb0fa0dd4c1153c64b73fd26
sleduardo20 <sl.eduardo20@gmail.com>
2021-04-29 00:17:20 -0300

    📄 docs (CartList component): alter story, add args

--- src/stories/26-CartList.stories.tsx
+++ src/stories/26-CartList.stories.tsx
@@ -1,16 +1,16 @@
 import { Story, Meta } from '@storybook/react/types-6-0';
 
-import CardList, { CartListProps } from '../components/CartList';
-import mockCardList from '../components/CartList/mock';
+import CardList from '../components/CartList';
+import items from '../components/CartList/mock';
 
 export default {
   title: 'components/CardList',
   component: CardList,
-  args: {
-    items: mockCardList,
-    total: 'R$ 330,00',
-  },
+
   argTypes: {
+    cartContextValue: {
+      type: '',
+    },
     items: {
       type: '',
     },
@@ -22,19 +22,29 @@ export default {
   },
 } as Meta;
 
-export const Basic: Story<CartListProps> = args => (
+export const Basic: Story = args => (
   <div style={{ maxWidth: '800' }}>
     <CardList {...args} />
   </div>
 );
 
-export const WithButton: Story<CartListProps> = args => (
+Basic.args = {
+  total: 'R$ 330,00',
+  cartContextValue: { items },
+};
+
+export const WithButton: Story = args => (
   <div style={{ maxWidth: '800' }}>
     <CardList hasButton {...args} />
   </div>
 );
 
-export const Empty: Story<CartListProps> = args => (
+WithButton.args = {
+  total: 'R$ 330,00',
+  cartContextValue: { items },
+};
+
+export const Empty: Story = args => (
   <div style={{ maxWidth: '800' }}>
     <CardList />
   </div>
