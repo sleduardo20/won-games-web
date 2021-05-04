13fe2d4dc73291f95b0e50e1d84bf5d812647a36
sleduardo20 <sl.eduardo20@gmail.com>
2021-05-04 13:09:59 -0300

    ✨ feat (FormSignIn component): add hook signIn the next-auth

--- src/components/FormSignIn/index.tsx
+++ src/components/FormSignIn/index.tsx
@@ -1,4 +1,8 @@
+import { UsersPermissionsRegisterInput } from 'graphql/generated/globalTypes';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { useState } from 'react';
+import { signIn } from 'next-auth/client';
 import { Email, Lock } from 'styled-icons/material-outlined';
 
 import Button from '../Button';
@@ -7,32 +11,57 @@ import TextField from '../TextField';
 import { Container, ForgotPassword, FormLink } from './styles';
 
 const FormSignIn = () => {
+  const [values, setValues] = useState({});
+
+  const { push } = useRouter();
+
+  const handleInput = (field: string, value: string) => {
+    setValues(state => ({ ...state, [field]: value }));
+  };
+
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+
+    const result = await signIn('credentials', {
+      ...values,
+      redirect: false,
+      callbackUrl: `${process.env.NEXT_PUBLIC_API_URL}/`,
+    });
+
+    if (result?.url) {
+      return push(result?.url);
+    }
+
+    console.log('email ou senha invalida');
+  };
   return (
     <Container>
-      <form>
+      <form onSubmit={handleSubmit}>
         <TextField
           name="email"
           placeholder="Email"
           type="email"
+          onInputChange={value => handleInput('email', value)}
           icon={<Email />}
         />
         <TextField
           name="password"
-          placeholder="Senha"
+          placeholder="Password"
           type="password"
+          onInputChange={value => handleInput('password', value)}
           icon={<Lock />}
         />
+        <ForgotPassword href="#">Forgot your password ?</ForgotPassword>
+        <Button size="large" fullWidth type="submit">
+          Sign In now
+        </Button>
+        <FormLink>
+          Don´t have an account ?
+          <Link href="/sign-up">
+            <a>Sign up</a>
+          </Link>
+        </FormLink>
       </form>
-      <ForgotPassword href="#">Esqueceu sua senha ?</ForgotPassword>
-      <Button size="large" fullWidth>
-        Entrar
-      </Button>
-      <FormLink>
-        Ainda não tem conta ?
-        <Link href="/sign-up">
-          <a>Cadastre-se</a>
-        </Link>
-      </FormLink>
     </Container>
   );
 };
