159813059495a99c32afd1e6a8b0dcb0511f9b94
sleduardo20 <sl.eduardo20@gmail.com>
2021-05-04 11:47:22 -0300

    ✨ feat (Auth): config next auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,11 @@
-import { ApolloProvider } from '@apollo/client';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
-import NextProgressBar from 'nextjs-progressbar';
 
+import { ApolloProvider } from '@apollo/client';
+import { Provider as NextAuthProvider } from 'next-auth/client';
 import { ThemeProvider } from 'styled-components';
+
+import NextProgressBar from 'nextjs-progressbar';
 
 import { useApollo } from 'utils/apollo';
 
@@ -17,31 +19,33 @@ function App({ Component, pageProps }: AppProps) {
   const client = useApollo(pageProps.initialApolloState);
 
   return (
-    <ApolloProvider client={client}>
-      <ThemeProvider theme={theme}>
-        <CartProvider>
-          <Head>
-            <title>Won Games</title>
-            <link rel="shortcut icon" href="/img/icon-512.png" />
-            <link rel="apple-touch-icon" href="/img/icon-512.png" />
-            <link rel="manifest" href="/manifest.json" />
-            <meta
-              name="description"
-              content="The best Game Stories in the world!"
-            />
-          </Head>
+    <NextAuthProvider session={pageProps.session}>
+      <ApolloProvider client={client}>
+        <ThemeProvider theme={theme}>
+          <CartProvider>
+            <Head>
+              <title>Won Games</title>
+              <link rel="shortcut icon" href="/img/icon-512.png" />
+              <link rel="apple-touch-icon" href="/img/icon-512.png" />
+              <link rel="manifest" href="/manifest.json" />
+              <meta
+                name="description"
+                content="The best Game Stories in the world!"
+              />
+            </Head>
 
-          <Component {...pageProps} />
-          <NextProgressBar
-            color="#f231a5"
-            startPosition={0.3}
-            stopDelayMs={200}
-            height={3}
-          />
-          <GlobalStyles />
-        </CartProvider>
-      </ThemeProvider>
-    </ApolloProvider>
+            <Component {...pageProps} />
+            <NextProgressBar
+              color="#f231a5"
+              startPosition={0.3}
+              stopDelayMs={200}
+              height={3}
+            />
+            <GlobalStyles />
+          </CartProvider>
+        </ThemeProvider>
+      </ApolloProvider>
+    </NextAuthProvider>
   );
 }
 
---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,76 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import NextAuth from 'next-auth';
+import Providers from 'next-auth/providers';
+
+type Session = {
+  jwt: string | null;
+  id: string | null;
+};
+type User = {
+  id: string | null;
+  name: string | null;
+  email: string | null;
+  jwt: string | null;
+};
+type Token = {
+  id: string | null;
+  email: string | null;
+  name: string | null;
+  jwt: string | null;
+};
+type Authorize = {
+  email: string;
+  password: string;
+};
+
+const options = {
+  pages: {
+    signIn: '/sing-in',
+  },
+  providers: [
+    Providers.Credentials({
+      name: 'Sing-In',
+      credentials: {},
+      async authorize({ email, password }: Authorize) {
+        const response = await fetch(
+          `${process.env.NEXT_PUBLIC_API_URL}/auth/local`,
+          {
+            method: 'POST',
+            body: new URLSearchParams({ indentifier: email, password }),
+          },
+        );
+
+        const data = await response.json();
+
+        if (data.user) {
+          return { ...data.user, jwt: data.jwt };
+        }
+        return null;
+      },
+    }),
+  ],
+  callbacks: {
+    session: async (session: Session, user: User) => {
+      session.jwt = user.jwt;
+      session.id = user.id;
+
+      return Promise.resolve(session);
+    },
+    jwt: async (token: Token, user: User) => {
+      if (user) {
+        token.id = user.id;
+        token.email = user.email;
+        token.name = user.name;
+        token.jwt = user.jwt;
+      }
+
+      return Promise.resolve(token);
+    },
+  },
+};
+
+const Auth = (request: NextApiRequest, response: NextApiResponse) => {
+  NextAuth(request, response, options);
+};
+
+export default Auth;
