0d6d2adbe01ef0fb9c6958c6ccee9e05edc9ea15
sleduardo20 <sl.eduardo20@gmail.com>
2021-05-04 09:15:43 -0300

    ✔ tests (FormSignUp component): add MockedProvider

--- src/components/FormSignUp/formSignUp.spec.tsx
+++ src/components/FormSignUp/formSignUp.spec.tsx
@@ -1,11 +1,16 @@
+import { MockedProvider } from '@apollo/client/testing';
 import FormSignUp from 'components/FormSignUp';
 import { screen, render } from '../../utils/test-utils';
 
 describe('<FormSignUp />', () => {
   it('should be able render form correctly', () => {
-    const { container } = render(<FormSignUp />);
+    const { container } = render(
+      <MockedProvider>
+        <FormSignUp />
+      </MockedProvider>,
+    );
 
-    expect(screen.getByPlaceholderText('Nome')).toBeInTheDocument();
+    expect(screen.getByPlaceholderText('Username')).toBeInTheDocument();
     expect(screen.getByPlaceholderText('Email')).toBeInTheDocument();
     expect(screen.getByPlaceholderText('Senha')).toBeInTheDocument();
     expect(screen.getByPlaceholderText('Confirmar senha')).toBeInTheDocument();
@@ -17,7 +22,11 @@ describe('<FormSignUp />', () => {
   });
 
   it('should be able render the text and link to sign in', () => {
-    render(<FormSignUp />);
+    render(
+      <MockedProvider>
+        <FormSignUp />
+      </MockedProvider>,
+    );
 
     expect(screen.getByText(/Já tem uma conta?/i)).toBeInTheDocument();
 
