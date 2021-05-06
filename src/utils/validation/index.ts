98b03212f0220fdacfd48f5597c9f096ca2b7a98
sleduardo20 <sl.eduardo20@gmail.com>
2021-05-06 09:21:50 -0300

    ✨ feat (Utils, validation): create utils validation forms with lib Joi

---
+++ src/utils/validation/index.ts
@@ -0,0 +1,45 @@
+import { UsersPermissionsRegisterInput } from 'graphql/generated/globalTypes';
+import Joi from 'joi';
+
+type SignInValues = Omit<UsersPermissionsRegisterInput, 'username'>;
+
+const fieldsValidations = {
+  username: Joi.string().min(5).required(),
+  email: Joi.string()
+    .email({ tlds: { allow: false } })
+    .required(),
+  password: Joi.string().required(),
+  confirm_password: Joi.string()
+    .required()
+    .valid(Joi.ref('password'))
+    .messages({ 'any.only': ' confirm password does not match with password' }),
+};
+
+export type FieldErrors = {
+  [key: string]: string;
+};
+function getFieldErrors(objError: Joi.ValidationResult) {
+  const errors: FieldErrors = {};
+
+  if (objError.error) {
+    objError.error.details.forEach(err => {
+      errors[err.path.join('.')] = err.message;
+    });
+  }
+
+  return errors;
+}
+
+export function signInValidate(values: SignInValues) {
+  const { email, password } = fieldsValidations;
+
+  const schema = Joi.object({ email, password });
+
+  return getFieldErrors(schema.validate(values, { abortEarly: false }));
+}
+
+export function signUpValidate(values: SignInValues) {
+  const schema = Joi.object(fieldsValidations);
+
+  return getFieldErrors(schema.validate(values, { abortEarly: false }));
+}
