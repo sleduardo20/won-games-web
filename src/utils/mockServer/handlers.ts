c7212a61fb37aecc1724a9522c63965b0f335311
sleduardo20 <sl.eduardo20@gmail.com>
2021-05-07 11:58:26 -0300

    ✨ feat (MockServer): create handlers

--- src/utils/mockServer/handlers.ts
+++ src/utils/mockServer/handlers.ts
@@ -1,4 +1,35 @@
 import { rest } from 'msw';
 
+type LoginReqBody = {
+  email: string;
+};
+
 // interceptar as chamadas
-export const handlers = [];
+export const handlers = [
+  rest.post<LoginReqBody>(
+    `${process.env.NEXT_PUBLIC_API_URL}/auth/forgot-password`,
+    (req, res, ctx) => {
+      const { email } = req.body;
+
+      if (email === 'false@email.com') {
+        return res(
+          ctx.status(400),
+          ctx.json({
+            error: 'Bad Request',
+            messege: [
+              {
+                message: [
+                  {
+                    message: 'This email does not exist',
+                  },
+                ],
+              },
+            ],
+          }),
+        );
+      }
+
+      return res(ctx.status(200), ctx.json({ ok: true }));
+    },
+  ),
+];
