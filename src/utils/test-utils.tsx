01e7ab9d75c43ae82c4e7d48200a52161cbb0a02
sleduardo20 <sl.eduardo20@gmail.com>
2021-05-12 09:07:49 -0300

    ✨ feat (Utils): add WishlistContextProvider

--- src/utils/test-utils.tsx
+++ src/utils/test-utils.tsx
@@ -1,28 +1,40 @@
 import { render, RenderOptions } from '@testing-library/react';
+
 import {
   CartContext,
   CartContextData,
   defaultValuesCartContext,
 } from 'hooks/useCart';
+
+import {
+  WishListContext,
+  WishListContextData,
+  wishListContextDefaulValues,
+} from 'hooks/useWishList';
+
 import { ReactElement } from 'react';
 import { ThemeProvider } from 'styled-components';
 import theme from 'styles/theme';
 
 interface CustomRenderProps extends Omit<RenderOptions, 'queries'> {
   cartProviderProps?: CartContextData;
+  wishlistProviderProps?: WishListContextData;
 }
 
 const customRender = (
   ui: ReactElement,
   {
     cartProviderProps = defaultValuesCartContext,
+    wishlistProviderProps = wishListContextDefaulValues,
     ...renderOptions
   }: CustomRenderProps = {},
 ) =>
   render(
     <ThemeProvider theme={theme}>
       <CartContext.Provider value={cartProviderProps}>
-        {ui}
+        <WishListContext.Provider value={wishlistProviderProps}>
+          {ui}
+        </WishListContext.Provider>
       </CartContext.Provider>
     </ThemeProvider>,
     renderOptions,
