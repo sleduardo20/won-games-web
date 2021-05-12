fb28ba2a7e558c08173b9c0d7e11453a53ccc97a
sleduardo20 <sl.eduardo20@gmail.com>
2021-05-12 09:40:01 -0300

    ✨ feat (WishlistButtom component): create structure

---
+++ src/components/WishlistButton/index.tsx
@@ -0,0 +1,38 @@
+import { Favorite, FavoriteBorder } from '@styled-icons/material-outlined';
+import Button, { ButtonProps } from 'components/Button';
+import { useWishList } from 'hooks/useWishList';
+import { useSession } from 'next-auth/client';
+
+interface WishlistButtonProps extends Pick<ButtonProps, 'size'> {
+  id: string;
+  hasText?: boolean;
+}
+
+const WishlistButton = ({ id, hasText, size }: WishlistButtonProps) => {
+  const [session] = useSession();
+  const { isInWishList } = useWishList();
+  const buttonText = isInWishList(id)
+    ? 'Remove from Wishlist'
+    : 'Add to Wishlist';
+
+  if (!session) {
+    return null;
+  }
+  return (
+    <Button
+      minimal
+      size={size}
+      icon={
+        isInWishList(id) ? (
+          <Favorite aria-label={buttonText} />
+        ) : (
+          <FavoriteBorder aria-label={buttonText} />
+        )
+      }
+    >
+      {buttonText}
+    </Button>
+  );
+};
+
+export default WishlistButton;
