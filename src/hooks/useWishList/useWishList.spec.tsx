ed94d544a49f18aa9a7844abedd146b9f5fb642d
sleduardo20 <sl.eduardo20@gmail.com>
2021-05-12 09:00:51 -0300

    ✅ tests (Hook useWishlist): add tests add and remove items in wishlist

--- src/hooks/useWishList/useWishList.spec.tsx
+++ src/hooks/useWishList/useWishList.spec.tsx
@@ -1,20 +1,28 @@
 import { MockedProvider } from '@apollo/client/testing';
+
 import { renderHook } from '@testing-library/react-hooks';
+import { act, waitFor } from '../../utils/test-utils';
 
 import { WishListProvider, useWishList } from '.';
-import { wishListMock, wishListItems } from './mock';
+import {
+  wishlistMock,
+  wishlistItems,
+  createWishlistMock,
+  updateWishlistMock,
+  removeWishlistMock,
+} from './mock';
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const useSession = jest.spyOn(require('next-auth/client'), 'useSession');
 
-const session = { jwt: '123', user: { email: 'loremyspon@email.com' } };
+const session = { jwt: '123', user: { email: 'lorem@ipsum.com' } };
 
 useSession.mockImplementation(() => [session]);
 
 describe('useWishList', () => {
   it('should be able return wishlist items', async () => {
     const wrapper = ({ children }: { children: React.ReactNode }) => (
-      <MockedProvider mocks={[wishListMock]}>
+      <MockedProvider mocks={[wishlistMock]}>
         <WishListProvider>{children}</WishListProvider>
       </MockedProvider>
     );
@@ -28,14 +36,14 @@ describe('useWishList', () => {
     await waitForNextUpdate();
 
     expect(result.current.items).toStrictEqual([
-      wishListItems[0],
-      wishListItems[1],
+      wishlistItems[0],
+      wishlistItems[1],
     ]);
   });
 
   it('should be able check if the game is in wishlist', async () => {
     const wrapper = ({ children }: { children: React.ReactNode }) => (
-      <MockedProvider mocks={[wishListMock]}>
+      <MockedProvider mocks={[wishlistMock]}>
         <WishListProvider>{children}</WishListProvider>
       </MockedProvider>
     );
@@ -49,5 +57,68 @@ describe('useWishList', () => {
     expect(result.current.isInWishList('1')).toBe(true);
     expect(result.current.isInWishList('2')).toBe(true);
     expect(result.current.isInWishList('3')).toBe(false);
+  });
+
+  it('should be able add item in wishlist creating a new list', async () => {
+    const wrapper = ({ children }: { children: React.ReactNode }) => (
+      <MockedProvider mocks={[createWishlistMock]}>
+        <WishListProvider>{children}</WishListProvider>
+      </MockedProvider>
+    );
+
+    const { result, waitForNextUpdate } = renderHook(() => useWishList(), {
+      wrapper,
+    });
+
+    act(() => {
+      result.current.addToWishList('3');
+    });
+
+    await waitForNextUpdate();
+
+    expect(result.current.items).toStrictEqual([wishlistItems[2]]);
+  });
+
+  it('should be able add item in wishlist updating the current list', async () => {
+    const wrapper = ({ children }: { children: React.ReactNode }) => (
+      <MockedProvider mocks={[wishlistMock, updateWishlistMock]}>
+        <WishListProvider>{children}</WishListProvider>
+      </MockedProvider>
+    );
+
+    const { result, waitForNextUpdate } = renderHook(() => useWishList(), {
+      wrapper,
+    });
+
+    await waitForNextUpdate();
+
+    act(() => {
+      result.current.addToWishList('3');
+    });
+
+    await waitFor(() => {
+      expect(result.current.items).toStrictEqual(wishlistItems);
+    });
+  });
+  it('should be able remove item from wishlist', async () => {
+    const wrapper = ({ children }: { children: React.ReactNode }) => (
+      <MockedProvider mocks={[wishlistMock, removeWishlistMock]}>
+        <WishListProvider>{children}</WishListProvider>
+      </MockedProvider>
+    );
+
+    const { result, waitForNextUpdate } = renderHook(() => useWishList(), {
+      wrapper,
+    });
+
+    await waitForNextUpdate();
+
+    act(() => {
+      result.current.removeFromWishList('1');
+    });
+
+    await waitFor(() => {
+      expect(result.current.items).toStrictEqual([wishlistItems[1]]);
+    });
   });
 });
