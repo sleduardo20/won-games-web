f93af26730b43c4f95b945e092e3c4473f2b6614
sleduardo20 <sl.eduardo20@gmail.com>
2021-05-18 08:21:16 -0300

    ✨ feat (PaymentForm): add feature payload from stripe

--- src/components/PaymentForm/index.tsx
+++ src/components/PaymentForm/index.tsx
@@ -1,13 +1,14 @@
 import { useEffect, useState } from 'react';
 import { Session } from 'next-auth';
 
-import { CardElement } from '@stripe/react-stripe-js';
 import { StripeCardElementChangeEvent } from '@stripe/stripe-js';
+import { CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
 
 import { ErrorOutline, ShoppingCart } from '@styled-icons/material-outlined';
 
 import { useCart } from 'hooks/useCart';
 import { createPaymentIntent } from 'utils/stripe/methods';
+import { FormLoading } from 'components/FormForgotPassword/styles';
 import Button from '../Button';
 import Heading from '../Heading';
 
@@ -19,9 +20,13 @@ type PaymentFormProps = {
 
 const PaymentForm = ({ session }: PaymentFormProps) => {
   const [error, setError] = useState<string | null>(null);
+  const [loading, setLoading] = useState(false);
   const [disabled, setDisabled] = useState(true);
   const [clientSecret, setClientSecret] = useState('');
   const [freeGames, setFreeGames] = useState(false);
+
+  const stripe = useStripe();
+  const elements = useElements();
 
   const { items } = useCart();
 
@@ -54,43 +59,66 @@ const PaymentForm = ({ session }: PaymentFormProps) => {
     setError(event.error ? event.error.message : '');
     setDisabled(event.empty);
   };
+
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+    setLoading(true);
+
+    const payload = await stripe!.confirmCardPayment(clientSecret, {
+      payment_method: {
+        card: elements!.getElement(CardElement),
+      },
+    });
+    if (payload.error) {
+      setError(`Payment failed ${payload.error.message}`);
+      setLoading(false);
+    } else {
+      setError(null);
+      setLoading(false);
+
+      console.log('deu certo');
+    }
+  };
   return (
     <Container>
-      <Body>
-        <Heading color="black" size="small" lineBottom>
-          Payment
-        </Heading>
+      <form onSubmit={handleSubmit}>
+        <Body>
+          <Heading color="black" size="small" lineBottom>
+            Payment
+          </Heading>
 
-        {freeGames ? (
-          <FreeGames>Only free games, click buy and enjoy!</FreeGames>
-        ) : (
-          <CardElement
-            options={{
-              hidePostalCode: true,
-              style: { base: { fontSize: '16px' } },
-            }}
-            onChange={handleChange}
-          />
-        )}
+          {freeGames ? (
+            <FreeGames>Only free games, click buy and enjoy!</FreeGames>
+          ) : (
+            <CardElement
+              options={{
+                hidePostalCode: true,
+                style: { base: { fontSize: '16px' } },
+              }}
+              onChange={handleChange}
+            />
+          )}
 
-        {error && (
-          <Error>
-            <ErrorOutline size={16} style={{ marginRight: 4 }} /> {error}
-          </Error>
-        )}
-      </Body>
-      <Footer>
-        <Button as="a" fullWidth minimal>
-          Continue shopping
-        </Button>
-        <Button
-          fullWidth
-          icon={<ShoppingCart />}
-          disabled={!freeGames && (disabled || !!error)}
-        >
-          Buy Now
-        </Button>
-      </Footer>
+          {error && (
+            <Error>
+              <ErrorOutline size={16} style={{ marginRight: 4 }} /> {error}
+            </Error>
+          )}
+        </Body>
+        <Footer>
+          <Button as="a" fullWidth minimal>
+            Continue shopping
+          </Button>
+          <Button
+            type="submit"
+            fullWidth
+            icon={loading ? <FormLoading /> : <ShoppingCart />}
+            disabled={!freeGames && (disabled || !!error)}
+          >
+            {!loading && <span>Buy Now</span>}
+          </Button>
+        </Footer>
+      </form>
     </Container>
   );
 };
