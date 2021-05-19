18b5bf713ede4d9b63366bf288033e30932c1e5f
sleduardo20 <sl.eduardo20@gmail.com>
2021-05-19 10:34:25 -0300

    ✨ feat (Mappers): create mapper ordersMapper

--- src/utils/mappers/index.ts
+++ src/utils/mappers/index.ts
@@ -1,4 +1,5 @@
 import { QueryWishList_wishlists_games } from 'graphql/generated/QueryWishList';
+import { QueryOrders_orders } from 'graphql/generated/QueryOrders';
 import {
   QueryHome_banners,
   QueryHome_sections_popularGames_highlight,
@@ -63,5 +64,35 @@ export const cartMapper = (games: QueryGames_games[] | undefined) => {
         img: `http://localhost:1337${game.cover?.url}`,
         price: formatPrice(game.price),
       }))
+    : [];
+};
+
+export const ordersMapper = (orders: QueryOrders_orders[]) => {
+  return orders
+    ? orders.map(order => {
+        return {
+          id: order.id,
+          paymentInfo: {
+            flag: order.card_brand,
+            img: order.card_brand ? `/img/cards/${order.card_brand}.png` : null,
+            number: order.card_last4
+              ? `**** **** **** ${order.card_last4}`
+              : 'Free Game',
+            purchaseDate: `Purchase made on ${new Intl.DateTimeFormat('en-US', {
+              day: 'numeric',
+              month: 'short',
+              year: 'numeric',
+            }).format(new Date(order.created_at))}`,
+          },
+          games: order.games.map(game => ({
+            id: game.id,
+            title: game.name,
+            downloadLink:
+              'https://wongames.com/game/download/yuYT56Tgh431LkjhNBgdf',
+            img: `http://localhost:1337${game.cover?.url}`,
+            price: formatPrice(game.price),
+          })),
+        };
+      })
     : [];
 };
