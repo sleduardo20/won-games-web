32dd739fd21bc000fd875839468025a581c0d432
sleduardo20 <sl.eduardo20@gmail.com>
2021-05-19 11:04:38 -0300

    ✨ feat (Page Order): make the facth from orders the api

--- src/pages/profile/orders.tsx
+++ src/pages/profile/orders.tsx
@@ -1,11 +1,16 @@
 import { GetServerSideProps, GetServerSidePropsContext } from 'next';
+import protectedRoutes from 'utils/protected-routes';
+
+import { initializeApollo } from 'utils/apollo';
+import {
+  QueryOrders,
+  QueryOrdersVariables,
+} from 'graphql/generated/QueryOrders';
 
 import OrdersList, { OrdersListProps } from 'components/OrdersList';
-
-import protectedRoutes from 'utils/protected-routes';
+import { QUERY_ORDERS } from 'graphql/queries/orders';
+import { ordersMapper } from 'utils/mappers';
 import Profile from '../../templates/Profile';
-
-import itemsMock from '../../components/OrdersList/mock';
 
 export default function Orders({ items }: OrdersListProps) {
   return (
@@ -19,9 +24,18 @@ export const getServerSideProps: GetServerSideProps = async (
   context: GetServerSidePropsContext,
 ) => {
   const session = await protectedRoutes(context);
+  const apolloClient = initializeApollo(null, session);
+
+  const { data } = await apolloClient.query<QueryOrders, QueryOrdersVariables>({
+    query: QUERY_ORDERS,
+    variables: {
+      identifier: `${session?.id}`,
+    },
+  });
+
   return {
     props: {
-      items: itemsMock,
+      items: ordersMapper(data.orders),
       session,
     },
   };
