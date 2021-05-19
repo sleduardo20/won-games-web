b1c65b441b71e6899dc9d2d48984f6de0fd30085
sleduardo20 <sl.eduardo20@gmail.com>
2021-05-19 12:31:41 -0300

    ✅ test (PaymentForm): add tests

---
+++ src/components/PaymentForm/paymentForm.spec.tsx
@@ -0,0 +1,98 @@
+import { CartContextData, defaultValuesCartContext } from 'hooks/useCart';
+import { Session } from 'next-auth';
+import { render, screen, waitFor } from 'utils/test-utils';
+import * as stripeMethods from '../../utils/stripe/methods';
+
+import PaymentForm from '.';
+import items from '../CartList/mock';
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const useRouter = jest.spyOn(require('next/router'), 'useRouter');
+
+useRouter.mockImplementation(() => ({
+  push: jest.fn(),
+}));
+
+// Mock next Link
+jest.mock('next/link', () => ({
+  __esModule: true,
+  default: function Mock({ children }: { children: React.ReactNode }) {
+    return <div>{children}</div>;
+  },
+}));
+
+// Mock Stripe js
+jest.mock('@stripe/react-stripe-js', () => ({
+  CardElement: function Mock({ children }: { children: React.ReactNode }) {
+    return <div data-testid="Mock CardElement">{children}</div>;
+  },
+  Elements: function Mock({ children }: { children: React.ReactNode }) {
+    return <div data-testid="Mock Elements">{children}</div>;
+  },
+  useStripe: jest.fn().mockReturnValue({
+    confirmCardPayment: jest.fn().mockResolvedValue({
+      paymentMethod: {
+        card: 'card',
+      },
+    }),
+  }),
+  useElements: jest.fn().mockResolvedValue({
+    getElement: jest.fn(),
+  }),
+}));
+
+// create mock to createPaymentIntent method
+const createPaymentIntent = jest.spyOn(stripeMethods, 'createPaymentIntent');
+
+describe('<PaymentForm />', () => {
+  let session: Session;
+  let cartProviderProps: CartContextData;
+
+  beforeEach(() => {
+    session = {
+      jwt: 'token',
+      user: {
+        email: 'won@gamail.com',
+      },
+      expires: '123',
+    };
+    cartProviderProps = {
+      ...defaultValuesCartContext,
+      items,
+    };
+  });
+
+  it('should be able render component correctly', () => {
+    render(<PaymentForm session={session} />);
+
+    expect(screen.getByRole('heading', { name: /Payment/i })).toBeInTheDocument;
+    expect(screen.getByTestId(/Mock CardElement/i)).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: /buy now/i })).toBeDisabled();
+  });
+
+  it('should be able call create payment when it renders and render free it gets freeGames', async () => {
+    createPaymentIntent.mockResolvedValueOnce({ freeGames: true });
+
+    render(<PaymentForm session={session} />, { cartProviderProps });
+
+    expect(createPaymentIntent).toHaveBeenCalled();
+
+    await waitFor(() => {
+      expect(
+        screen.getByText(/Only free games, click buy and enjoy!/i),
+      ).toBeInTheDocument();
+    });
+  });
+
+  it('should call createPayment when it renders and render error if has any issue', async () => {
+    createPaymentIntent.mockResolvedValueOnce({ error: 'Error message' });
+
+    render(<PaymentForm session={session} />, { cartProviderProps });
+
+    expect(createPaymentIntent).toHaveBeenCalled();
+
+    await waitFor(() => {
+      expect(screen.getByText(/Error message/i)).toBeInTheDocument();
+    });
+  });
+});
