04124fdd2a2d662717acb7be4d4ab9f011c0e116
sleduardo20 <sl.eduardo20@gmail.com>
2021-05-27 11:54:19 -0300

    ♻ refactor (E2E): create command shoudRenderBannser

--- cypress/e2e/home.ts
+++ cypress/e2e/home.ts
@@ -2,25 +2,9 @@
 
 describe('Home Page', () => {
   it('should be able render sections', () => {
-    //visitar a pagina
+    
     cy.visit('/');
-
-    //selecionou os banners
-    cy.get('.slick-slider').within(() => {
-      cy.findByRole('heading', { name: /The Witcher 3/i, level: 2} )
-      cy.findByRole('link', { name: /buy now/i} )
-      
-      cy.get('.slick-dots > :nth-child(2) > button').click()
-      cy.wait(500)
-
-      cy.findByRole('heading', { name: /Cyberpunk 2077/i, level: 2} )
-      cy.findByRole('link', { name: /buy now/i} )
-
-      cy.get('.slick-dots > :nth-child(3) > button').click()
-      
-
-      cy.findByRole('heading', { name: /Fallout 3: Game of the Year Edition/i, level: 2})
-      cy.findByRole('link', { name: /buy now/i} )
-    });
+    
+    cy.shouldRenderBanner();
   });
 });
--- cypress/support/index.d.ts
+++ cypress/support/index.d.ts
@@ -6,5 +6,9 @@ declare namespace Cypress {
     // Custom command to visit google page
     // @example cy.google()
     google(): Chainable<Window>;
+    
+    // Custom command to check banner in page
+    // @example cy.shouldRenderBanner()
+    shouldRenderBanner(): Chainable<Element>;
   }
 }
