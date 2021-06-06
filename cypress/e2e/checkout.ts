2de6dfc9f81ea9e19406d2e4f4a2f73ac754c5a1
sleduardo20 <sl.eduardo20@gmail.com>
2021-06-06 08:43:02 -0300

    ✅ test (e2e): create test checkout paiment games

--- cypress/e2e/checkout.ts
+++ cypress/e2e/checkout.ts
@@ -37,7 +37,7 @@ describe('Checkout', () => {
       });
 
       //encontrar um texto de só jogos free
-      cy.findByText(/only free games/i).should('exist');
+      cy.findByText(/Only free games/i).should('exist');
 
       //clicar em comprar
       cy.findByRole('button', {name: /Buy Now/i}).click();
@@ -63,6 +63,63 @@ describe('Checkout', () => {
   });
   
   describe('Paid Games', () => {
+    before(() => {
+      user = createUser();
+    });
 
+    it('should be able buy paid games', () => {
+      //criar um usuario
+      cy.visit('/sign-up');
+      cy.signUp(user);
+      cy.url().should('eq', `${Cypress.config().baseUrl}`);
+
+      // //ir para explore page
+      cy.findByRole('link', { name: /explore/i }).click();
+
+      
+      cy.url().should('eq', `${Cypress.config().baseUrl}games`);
+
+      // //filtrar por jogos pagos
+      cy.findByText(/highest to lowest/i).click();
+      cy.url().should('contain', '?sort=price%3Adesc');
+
+      // //adicionar um jogo free
+      cy.addToCart(0)
+
+      // //verificar se o carrinho tem o jogo free e abrir o dropdown
+      cy.findAllByLabelText(/cart items/i).first().should('have.text', 1).click();
+
+      // //clicar para fazer a compra
+      cy.getByDataCy('cart-list').within(() => {
+        cy.findByText(/buy it now/i).click();
+      });
+
+      //o botão de comprar deve esta desabilitado
+      cy.findByRole('button', { name: /buy now/i }).should('have.attr','disabled');
+
+      //preencher com o cartão de credito
+      cy.fillElementsInput('cardNumber','4242424242424242');
+      cy.fillElementsInput('cardExpiry','1030');
+      cy.fillElementsInput('cardCvc','123');
+      
+      //clicar em comprar
+      cy.findByRole('button', {name: /Buy Now/i}).click();
+  
+      //redirecionar para pagina de success
+      cy.url().should('eq', `${Cypress.config().baseUrl}success`);
+        
+      //mostrar o texto de sucesso
+      cy.findByRole('heading',{ name: /Your purchase was successful!/i }).should('exist');
+    });
+
+    it('should show games in order page', () => {
+      cy.visit('/profile/orders');
+      cy.location('href').should('eq', `${Cypress.config().baseUrl}sign-in?callbackUrl=/profile/orders`);
+
+      cy.signIn(user.email, user.password);
+      cy.location('href').should('eq', `${Cypress.config().baseUrl}profile/orders`);
+      cy.getByDataCy('game-item').should('have.length', 1);
+
+    });
   });
 });
